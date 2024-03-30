d408fc3ec2e2b35f97281661c61f089e649bfe87
Parshuram Kumar <yadavparshuram991@gmail.com>
2024-03-30 15:06:32 +0530

    responsive bar done + project section almost done

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const showMenu = () => {
-    console.log("clicked");
-    const navMobile = document.getElementsByClassName("navList-mobile")
-    navMobile.style.display = "flex"; 
+var elements = document.getElementsByClassName("navList-mobile");
+
+
+function show()  {
+    // console.log("clicked");
+    
+
+// Loop through each element and change its background color
+for (var i = 0; i < elements.length; i++) {
+    elements[i].style.display = "flex";
+}
 };
+// show();
+
+function hide() {
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = "none";
+    }
+}
+
